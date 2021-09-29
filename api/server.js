6c0e036b5012793eb3a7f7d6f0ebbfc2e596d0f2
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-09-29 15:46:17 -0700

    get all users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,37 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const Users = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+// [GET] /api/dogs (R of CRUD, fetch all dogs)
+server.get("/api/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      console.log(users);
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The users information could not be retrieved",
+        err: err.message,
+      });
+    });
+});
+
+// [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
+// server.post("/api/users", (req, res) => {
+//     const newUser = req.body
+//     if(!newUser.name || !newUser.bio) {
+//         res.status(400).json({
+//           message: "Please provide name and bio for the user",
+//         });
+//     } else Users.insert(newUser)
+//         .then(addUser => {
+//             res.status(201).json(addUser)
+//         })
+// })
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}

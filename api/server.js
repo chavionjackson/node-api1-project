275f53f9a547765ab3c2169c5c8e3a382f06bac8
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-09-29 18:37:39 -0700

    All request complete

--- api/server.js
+++ api/server.js
@@ -81,4 +81,30 @@ server.delete("/api/users/:id", async (req, res) => {
   }
 });
 
+// [PUT] /api/users/:id (U of CRUD, update user with :id using JSON payload)
+server.put("/api/users/:id", async (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    try {
+        if(!changes.name || !changes.bio) {
+            res.status(400).json({
+                message: "Please provide name and bio for the user"
+            })
+        } else {
+            const updatedUser = await Users.update(id, changes)
+            if(!updatedUser) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist",
+                })
+            } else {
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+          message: "The user information could not be modified",
+        });
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}

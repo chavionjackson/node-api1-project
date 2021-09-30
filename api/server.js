cd2d2ac4843ed5afc1d05395163b6a4663fb1431
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-09-29 17:19:44 -0700

    post complete

--- api/server.js
+++ api/server.js
@@ -24,7 +24,7 @@ server.get("/api/users", (req, res) => {
 
 // [GET] /api/users/:id (R of CRUD, fetch user by :id)
 server.get("/api/users/:id", (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   Users.findById(id)
     .then((user) => {
       if (!user) {
@@ -43,22 +43,23 @@ server.get("/api/users/:id", (req, res) => {
     });
 });
 
-// [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
-// server.post("/api/users", (req, res) => {
-//     const newUser = req.body
-//     if(!newUser.name || !newUser.bio) {
-//         res.status(400).json({
-//           message: "Please provide name and bio for the user",
-//         });
-//     } else Users.insert(newUser)
-//         .then(addUser => {
-//             res.status(201).json(addUser)
-//         })
-// })
+// [POST] /api/users (C of CRUD, create new user from JSON payload)
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  if (!newUser.name || !newUser.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    });
+  } else
+    Users.insert(newUser).then((addUser) => {
+      res.status(201).json(addUser);
+    })
+    .catch(err => {
+        res.status(500).json({
+          message: "There was an error while saving the user to the database",
+          err: err.message,
+        });
+    })
+});
 
-server.use("*", (req, res) => {
-  res.status(404).json({
-    message: "not found",
-  });
-}),
-  (module.exports = server); // EXPORT YOUR SERVER instead of {}
+module.exports = server; // EXPORT YOUR SERVER instead of {}

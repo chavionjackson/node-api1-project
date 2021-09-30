7af2bb7860b5c7ac09fdc02308177add9fbf63b1
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-09-29 18:13:10 -0700

    delete complete

--- api/server.js
+++ api/server.js
@@ -51,15 +51,34 @@ server.post("/api/users", (req, res) => {
       message: "Please provide name and bio for the user",
     });
   } else
-    Users.insert(newUser).then((addUser) => {
-      res.status(201).json(addUser);
-    })
-    .catch(err => {
+    Users.insert(newUser)
+      .then((addUser) => {
+        res.status(201).json(addUser);
+      })
+      .catch((err) => {
         res.status(500).json({
           message: "There was an error while saving the user to the database",
           err: err.message,
         });
-    })
+      });
+});
+
+// [DELETE] /api/users/:id (D of CRUD, remove user with :id)
+server.delete("/api/users/:id", async (req, res) => {
+  try {
+    const deletedUser = await Users.remove(req.params.id);
+    if (!deletedUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user could not be removed",
+    });
+  }
 });
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
